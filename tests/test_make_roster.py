49891cc4d370c6d2e540b535fe9c7f02f9822236
snehitha <mamidisnehitha@gmail.com>
2020-02-27 00:43:04 -0500

    Test case for test_make_roster

--- tests/test_make_roster.py
+++ tests/test_make_roster.py
@@ -23,11 +23,20 @@ class TestMakeRoster(unittest.TestCase):
         Test case for a 2-element roster
         """
         input1 = [
-            [901, 'adams'],
-            [902, 'bennett']
+            [901, 'adams','max'],
+            [902, 'bennett','nick']
         ]
         actual_result = self.sm.make_roster(input1)
-        expected_result = {901: 'adams', 902: 'bennett'}
+        expected_result = {901: ['adams','max'], 902: ['bennett','nick']}
+        self.assertDictEqual(actual_result, expected_result)
+
+    def test_case_element_roster(self):
+        """
+        Test case for make_roster
+        """
+        input1 = [[999,'Padidam','Pushyami']]
+        actual_result = self.sm.make_roster(input1)
+        expected_result = {999: ['Padidam','Pushyami']}
         self.assertDictEqual(actual_result, expected_result)
 
 
